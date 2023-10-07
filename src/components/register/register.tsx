80cd0492ca35ff4d6caedb91e4704ff7e8ad79fa
ecocle <146603117+ecocle@users.noreply.github.com>
2023-10-07 18:35:28 +0800

    Update register.tsx

--- src/components/register/register.tsx
+++ src/components/register/register.tsx
@@ -25,7 +25,7 @@ export const Register = ({ className, onClose, selectedLanguage }: RegisterProps
 
     const handleRegistration = async () => {
         try {
-            const response = await fetch('http://119.29.236.82/api/api/register', {
+            const response = await fetch('https://119.29.236.82/api/api/register', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
