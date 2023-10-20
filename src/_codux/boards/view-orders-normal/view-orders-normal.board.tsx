07b33715d4b89a87cece167badf3ec0c84d02885
ecocle <146603117+ecocle@users.noreply.github.com>
2023-10-21 00:34:10 +0800

    Remove all files except README.md

--- src/_codux/boards/view-orders-normal/view-orders-normal.board.tsx
+++
@@ -1,8 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import { ViewOrdersNormal } from '../../../components/view-orders-normal/view-orders-normal';
-
-export default createBoard({
-    name: 'ViewOrdersNormal',
-    Board: () => <ViewOrdersNormal />,
-    isSnippet: true,
-});
--- src/constants/constants.ts
+++
@@ -1,7 +0,0 @@
-export enum LANGUAGES {
-    ENGLISH = 'english',
-    CHINESE = 'chinese',
-}
-
-export const DEFAULT_LANGUAGE = LANGUAGES.ENGLISH;
-
